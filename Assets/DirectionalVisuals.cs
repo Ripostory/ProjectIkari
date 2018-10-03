122b8777dcbab8bc20939f522cb6bf5a4e942afe
Ripostory <ripostory@gmail.com>
2018-10-02 21:52:23 -0700

    Added player movement

---
+++ Assets/DirectionalVisuals.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionalVisuals : MonoBehaviour {
+    public KeyCode dive;
+    public Sprite diveSprite, downSprite, upSprite, rightSprite, leftSprite;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Rigidbody2D body = GetComponent<Rigidbody2D>();
+        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+
+        //flip on movement direction
+        if (body.angularVelocity < 0)
+        {
+            //sprite.flipY = false;
+        }
+        else
+        {
+            //sprite.flipY = true;
+        }
+
+        //choose sprite based on angular direction
+        float angle = Vector3.SignedAngle(new Vector3(1, 0, 0), transform.up, new Vector3(0, 0, 1));
+        if (angle > -45.0f && angle <= 45.0f)
+        {
+            sprite.sprite = rightSprite;
+        } else if(angle > 45.0f && angle <= 135.0f)
+        {
+            sprite.sprite = downSprite;
+        } else if ((angle > 135.0f && angle <= 180.0f) || (angle > -180.0f && angle <= -135.0f))
+        {
+            sprite.sprite = leftSprite;
+        } else
+        {
+            sprite.sprite = upSprite;
+        }
+
+        //if (Input.GetKey(dive))
+        //{
+        //    sprite.sprite = diveSprite;
+        //}
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public KeyCode left;
+    public KeyCode right;
+    public KeyCode dive;
+    public float power;
+    public float horizontalPower;
+    public float diveRotation;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rigidbody2D body = GetComponent<Rigidbody2D>();
+        float rotationState = 0.0f;
+        float direction = 0.0f;
+
+        //handle saving rotational state
+        if (Input.GetKeyDown(dive))
+        {
+            rotationState = body.rotation;
+        }
+
+        //get direction
+        if (body.velocity.x > 0)
+        {
+            direction = -1.0f;
+        }
+        else
+        {
+            direction = 1.0f;
+        }
+
+        //dive
+        if (!Input.GetKey(dive))
+        {
+            body.AddForce(new Vector2(0, power * Time.deltaTime));
+        }
+        else
+        {
+            body.angularVelocity = body.velocity.magnitude * diveRotation * direction;
+        }
+
+        //left right controls
+        if (Input.GetKey(right))
+        {
+            body.AddForce(new Vector2(horizontalPower * Time.deltaTime, 0));
+        }
+
+        if (Input.GetKey(left))
+        {
+            body.AddForce(new Vector2(-horizontalPower * Time.deltaTime, 0));
+        }
+    }
+}
