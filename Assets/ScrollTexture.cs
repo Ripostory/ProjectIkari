835bb9684a505f91896d3c2edc6bf7dfbe7e71da
Ripostory <ripostory@gmail.com>
2018-10-09 23:21:59 -0700

    More visual adjustments

---
+++ Assets/ScrollTexture.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollTexture : MonoBehaviour {
+    public float scrollX = 0.5f;
+    public float scrollY = 0.5f;
+
+    float internalScrollX = 0.0f;
+    float internalScrollY = 0.0f;
+
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        internalScrollX += Time.deltaTime * scrollX;
+        internalScrollY += Time.deltaTime * scrollY;
+        //keep scroll value bounded
+        internalScrollX %= 10;
+        internalScrollY %= 10;
+
+        GetComponent<Renderer>().material.mainTextureOffset = new Vector2(internalScrollX, internalScrollY);
+    }
+}
